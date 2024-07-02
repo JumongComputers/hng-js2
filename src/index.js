72101b34842d7256846155471d98a3b238c0508c
JumongComputers <jaytoluwase@gmail.com>
2024-07-02 22:42:24 +0100

    still on another hosting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const dotenv = require('dotenv');
+import fetch from 'node-fetch';
 
 dotenv.config();
 
